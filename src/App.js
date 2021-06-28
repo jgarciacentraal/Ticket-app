5b30373b86fa81fa250f9f2676784e07c304f294
Julio Estefanes <jsgr1992@gmail.com>
2021-06-28 18:43:00 -0500

    refactoring some components- class to function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-
-
-import Login from './components/Login.compo';
-import Ticket from './components/Ticket.compo'
-import Home from './pages/Home.page';
+import Login from './components/Login/Login';
+import Ticket from './components/Ticket/Ticket';
+import Home from './pages/Home';
 
 export default class App extends Component {
   render() {
     return (
-         <Router>
-           <div>
-             <Switch>
-               <Route exact path="/" component={Home} />
-               <Route path="/login" component={Login} />
-               <Route path="/ticket" component={Ticket}/>
-               <Route exact path="*" component={() => "404 NOT FOUND"} />
-             </Switch>
-           </div>
-         </Router>
-    )
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/ticket" component={Ticket} />
+            <Route exact path="*" component={() => '404 NOT FOUND'} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
-
 }
-
-
